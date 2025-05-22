35cfbfb37bd7f9c5c070e818bfc615b7b54c3ed0
Julia Zagaevskaya <u.zagaevskaya@gmail.com>
2025-05-22 23:03:59 +0300

    Работа над документацией. Отладка

---
+++ src/types/events.ts
@@ -0,0 +1,54 @@
+import { Product, CartData, Order } from "./index";  
+
+// События 
+
+// Перечисление событий
+// Список всех возможных событий, сгруппированных по функциональности
+export enum AppEvents {
+    // Товары
+    PRODUCTS_LOADED = 'products:loaded',                // Товары загружены (передается массив Product[])
+    PRODUCT_SELECTED = 'product:selected',              // Пользователь выбрал товар (передается Product)
+
+    // Корзина
+    CART_ITEM_ADDED = 'cart:item-added',                // Товар добавлен в корзину 
+    CART_ITEM_REMOVED = 'cart:item-removed',            // Товар удален из корзины 
+    CART_UPDATED = 'cart:updated',                      // Состояние корзины изменилось (передается CartData)
+
+    // Заказ
+    ORDER_STARTED = 'order:started',                    // Пользователь начал оформление заказа
+    ORDER_STEP1_COMPLETED = 'order:step1-completed',    // Пользователь заполнил адрес доставки и способ оплаты (завершен первый шаг)
+    ORDER_STEP2_COMPLETED = 'order:step2-completed',    // Пользователь заполнил контактные данные (завершен второй шаг)
+    ORDER_COMPLETED = 'order:completed',                // Заказ оформлен (передается Order)
+
+    // UI события
+    MODAL_OPENED = 'ui:modal-opened',                   // Открыто модальное окно (передается Product)
+    MODAL_CLOSED = 'ui:modal-closed'                    // Закрыто модальное окно
+}
+
+// Типы данных событий
+// Связывает событие с типом передаваемых данных
+export type EventMap = {
+    [AppEvents.PRODUCTS_LOADED]: { products: Product[]};
+    [AppEvents.PRODUCT_SELECTED]: { product: Product};
+    [AppEvents.CART_ITEM_ADDED]: { productId: string };
+    [AppEvents.CART_ITEM_REMOVED]: { productId: string };
+    [AppEvents.CART_UPDATED]: { cart: CartData };
+    [AppEvents.ORDER_STARTED]: undefined;
+    [AppEvents.ORDER_STEP1_COMPLETED]: { deliveryAddress: string, paymentMethod: string };
+    [AppEvents.ORDER_STEP2_COMPLETED]: { email: string, phone: string };
+    [AppEvents.ORDER_COMPLETED]: { order: Order};
+    [AppEvents.MODAL_OPENED]: { product: Product };
+    [AppEvents.MODAL_CLOSED]: undefined;
+}
+
+// Проверка того, что событие R существует в EventMap
+type _ApproveEventEntry = {
+    [R in AppEvents]: EventMap[R];
+}
+
+// Определяет методы для работы с событиями
+export interface IEventEmitter {
+    on<T extends AppEvents>(event: T, callback: (data: EventMap[T]) => void): void;     // Подписывает на событие
+    off<T extends AppEvents>(event: T, callback: (data: EventMap[T]) => void): void;    // Отписывает от события
+    emit<T extends AppEvents>(event: T, data: EventMap[T]): void;                       // Генерирует событие
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -169,45 +169,4 @@ interface ICheckoutView {
     showSuccess(): void;    // показывает сообщение об успешном оформлении заказа и очищает корзину
 }
 
-// События 
-
-// Перечисление событий
-// Список всех возможных событий, сгруппированных по функциональности
-enum AppEvents {
-    // Товары
-    PRODUCTS_LOADED = 'products:loaded',                // Товары загружены (передается массив Product[])
-    PRODUCT_SELECTED = 'product:selected',              // Пользователь выбрал товар (передается Product)
-
-    // Корзина
-    CART_ITEM_ADDED = 'cart:item-added',                // Товар добавлен в корзину 
-    CART_ITEM_REMOVED = 'cart:item-removed',            // Товар удален из корзины 
-    CART_UPDATED = 'cart:updated',                      // Состояние корзины изменилось (передается CartData)
-
-    // Заказ
-    ORDER_STARTED = 'order:started',                    // Пользователь начал оформление заказа
-    ORDER_STEP1_COMPLETED = 'order:step1-completed',    // Пользователь заполнил адрес доставки и способ оплаты (завершен первый шаг)
-    ORDER_STEP2_COMPLETED = 'order:step2-completed',    // Пользователь заполнил контактные данные (завершен второй шаг)
-    ORDER_COMPLETED = 'order:completed',                // Заказ оформлен (передается Order)
-
-    // UI события
-    MODAL_OPENED = 'ui:modal-opened',                   // Открыто модальное окно (передается Product)
-    MODAL_CLOSED = 'ui:modal-closed'                    // Закрыто модальное окно
-}
-
-// Типы данных событий
-// Связывает событие с типом передаваемых данных
-interface EventMap {
-    [AppEvents.PRODUCTS_LOADED]: { products: Product[]};
-    [AppEvents.PRODUCT_SELECTED]: { product: Product};
-    [AppEvents.CART_UPDATED]: { cart: CartData };
-    [AppEvents.ORDER_COMPLETED]: { order: Order};
-    [AppEvents.MODAL_OPENED]: { product: Product };
-    [AppEvents.MODAL_CLOSED]: { product: Product };
-}
-
-// Определяет методы для работы с событиями
-interface IEventEmitter {
-    on<T extends AppEvents>(event: T, callback: (data: EventMap[T]) => void): void;     // Подписывает на событие
-    off<T extends AppEvents>(event: T, callback: (data: EventMap[T]) => void): void;    // Отписывает от события
-    emit<T extends AppEvents>(event: T, data: EventMap[T]): void;                       // Генерирует событие
-}
+export { Product, CartData, Order}
